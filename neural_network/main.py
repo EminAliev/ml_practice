60b930a946f6788d23c8694a4946226310701dd0
EminAliev <samburer200@gmail.com>
2021-12-22 18:03:06 +0300

    edit nlr

--- neural_network/main.py
+++ neural_network/main.py
@@ -1,16 +1,15 @@
 import numpy as np
-import random
-import pygame
+import keras
 import tensorflow as tf
 from keras.models import load_model
-from enum import Enum
-from PIL import Image
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Flatten, Dense
-
-from const import Button, Colors
+from tensorflow.keras import layers
+import matplotlib.pyplot as plt
+import random
 
 dbname = 'mnist.keras'
+
+num_classes = 10
+input_shape = (28, 28, 1)
 try:
     f = open(dbname)
     f.close()
@@ -18,59 +17,76 @@ try:
     model_sequential = load_model(dbname)
 except IOError:
     print('creating db')
-    mnist = tf.keras.datasets.mnist
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-    x_train = tf.keras.utils.normalize(x_train, axis=1)
-    x_test = tf.keras.utils.normalize(x_test, axis=1)
-
-    model_sequential = Sequential()
-    model_sequential.add(Flatten())
-
-    model_sequential.add(Dense(256, activation=tf.nn.relu))
-    model_sequential.add(Dense(256, activation=tf.nn.relu))
-    model_sequential.add(Dense(10, activation=tf.nn.softmax))
-
-    model_sequential.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
-
-    model_sequential.fit(x=x_train, y=y_train, epochs=16, batch_size=32)
-
-    model_sequential.save(dbname)
-
-print(model_sequential.summary())
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
 
-def predict(screen):
-    path = 'digit.png'
-    pygame.image.save(screen, path)
-    digit_image = Image.open(path)
-    digit_image = digit_image.resize((28, 28))
-    digit_image = digit_image.convert('L')
-    digit_image = np.array(digit_image)
-    digit_image = digit_image.reshape(1, 28, 28, 1)
-    digit_image = digit_image / 255.0
-    prediction = model_sequential.predict([digit_image])[0]
-    res = prediction.argsort()[::-1]
-    print(res)
+x_train = x_train.astype("float32") / 255
+x_test = x_test.astype("float32") / 255
+
+x_train = np.expand_dims(x_train, -1)
+x_test = np.expand_dims(x_test, -1)
+print("x_train shape:", x_train.shape)
+print(x_train.shape[0], "train samples")
+print(x_test.shape[0], "test samples")
 
 
-screen = pygame.display.set_mode((280, 280))
-line_start = None
+y_train = tf.keras.utils.to_categorical(y_train, num_classes)
+y_test = tf.keras.utils.to_categorical(y_test, num_classes)
 
-while True:
-    for i in pygame.event.get():
-        if i.type == pygame.QUIT:
-            pygame.quit()
-            break
-        if i.type == pygame.MOUSEBUTTONDOWN:
-            if i.button == Button.Right.value:
-                predict(screen)
-        elif i.type == pygame.KEYDOWN and i.key == pygame.K_SPACE:
-            screen.fill(Colors.BLACK)
-    position = pygame.mouse.get_pos()
-    pressed = pygame.mouse.get_pressed()
-    if pressed[0]:
-        pygame.draw.circle(screen, Colors.WHITE,
-                           (position[0] + random.randrange(2), position[1] + random.randrange(2)),
-                           random.randrange(1, 5))
-    pygame.display.update()
+model = keras.Sequential(
+    [
+        keras.Input(shape=input_shape),
+        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Flatten(),
+        layers.Dropout(0.5),
+        layers.Dense(num_classes, activation="softmax"),
+    ]
+)
+
+model.summary()
+
+batch_size = 128
+epochs = 15
+
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+
+score = model.evaluate(x_test, y_test, verbose=0)
+
+
+def program_run(input_image):
+    digit_images = np.split(input_image, input_image.shape[1] / 28, axis=1)
+    digit_images = np.array(digit_images)
+
+
+    prediction_result = model.predict(digit_images)
+
+
+    ans = ""
+    for case in np.split(prediction_result, len(digit_images)):
+        predicted = np.argmax(case)
+        ans += str(predicted)
+
+    print(ans)
+
+
+def generate_input_image(possible_digits=x_test,
+                         input_length=4):
+    selected_digit_images = [possible_digits[random.randint(0, len(possible_digits))] for _ in range(input_length)]
+    concatenated = np.concatenate(selected_digit_images, axis=1)
+
+    plt.imshow(concatenated, cmap='gray')
+    plt.show()
+
+    return concatenated
+
+
+input_image = generate_input_image(input_length=4)
+
+program_run(input_image)
+
